bfc641eb3020e6c080dc4d9a1fd68a45d14b9b1e
gravity981 <gravity981@noreply.com>
2022-12-12 01:56:49 +0100

    implement TrafficModel and TrafficPage

--- gui/app/TrafficPage.qml
+++ gui/app/TrafficPage.qml
@@ -2,4 +2,9 @@ import QtQuick 2.15
 
 Page {
     title: "Traffic"
+    Text {
+        anchors.fill: parent
+        wrapMode: Text.WrapAnywhere
+        text: JSON.stringify(trafficModel)
+    }
 }
--- gui/app/main.py
+++ gui/app/main.py
@@ -16,6 +16,8 @@ def on_message(client, userdata, msg):
         updateSatellites(msg)
     elif msg.topic == "/easyadsb/json/position":
         updatePosition(msg)
+    elif msg.topic == "/easyadsb/json/traffic":
+        updateTraffic(msg)
 
 
 def updateSatellites(msg):
@@ -49,7 +51,6 @@ def updateSatellites(msg):
         svids = [s["svid"] for s in satellites]
         for oldSvId in oldSvIds:
             if oldSvId not in svids:
-                logger.info("remove " + str(oldSvId))
                 QMetaObject.invokeMethod(satellitesModel, "removeSatellite", Qt.QueuedConnection, Q_ARG(int, oldSvId))
     except Exception as ex:
         logger.error("could not parse satellites message, {}".format(str(ex)))
@@ -66,6 +67,24 @@ def updatePosition(msg):
         )
     except Exception as ex:
         logger.error("could not parse position message, {}".format(str(ex)))
+
+
+def updateTraffic(msg):
+    try:
+        raw = msg.payload.decode("utf-8").strip()
+        entries = json.loads(raw)
+        oldEntryIds = trafficModel.ids
+        for entry in entries:
+            if entry["id"] in oldEntryIds:
+                QMetaObject.invokeMethod(trafficModel, "updateTrafficEntry", Qt.QueuedConnection, Q_ARG(QVariant, entry))
+            else:
+                QMetaObject.invokeMethod(trafficModel, "addTrafficEntry", Qt.QueuedConnection, Q_ARG(QVariant, entry))
+        ids = [t["id"] for t in entries]
+        for oldId in oldEntryIds:
+            if oldId not in ids:
+                QMetaObject.invokeMethod(trafficModel, "removeTrafficEntry", Qt.QueuedConnection, Q_ARG(int, id))
+    except Exception as ex:
+        logger.error("could not parse traffic message, {}".format(str(ex)))
 
 
 class SatellitesModel(QAbstractListModel):
@@ -149,8 +168,9 @@ class SatellitesModel(QAbstractListModel):
 
     @pyqtSlot(int)
     def removeSatellite(self, svid):
+        logger.info("remove " + str(svid))
         row = self._rowFromSvId(svid)
-        self.beginRemoveColumns(QModelIndex(), row, row)
+        self.beginRemoveRows(QModelIndex(), row, row)
         del self._satellites[row]
         self.endRemoveRows()
 
@@ -260,12 +280,185 @@ class PositionModel(QObject):
         self.positionChanged.emit()
 
 
+class TrafficModel(QAbstractListModel):
+    IdRole = Qt.UserRole + 6
+    CallsignRole = Qt.UserRole + 7
+    ModelRole = Qt.UserRole + 8
+    CategoryRole = Qt.UserRole + 9
+    LatitudeRole = Qt.UserRole + 10
+    LongitudeRole = Qt.UserRole + 11
+    AltitudeRole = Qt.UserRole + 12
+    TrackRole = Qt.UserRole + 13
+    GroundSpeedRole = Qt.UserRole + 14
+    VerticalSpeedRole = Qt.UserRole + 15
+    SquawkRole = Qt.UserRole + 16
+    AlertRole = Qt.UserRole + 17
+    EmergencyRole = Qt.UserRole + 18
+    SpiRole = Qt.UserRole + 19
+    IsOnGroundRole = Qt.UserRole + 20
+    LastSeenRole = Qt.UserRole + 21
+    MsgCountRole = Qt.UserRole + 22
+
+    def __init__(self, parent=None):
+        QObject.__init__(self, parent)
+        self._trafficEntries = []
+
+    def data(self, index, role=Qt.DisplayRole):
+        row = index.row()
+        if role == TrafficModel.IdRole:
+            return self._trafficEntries[row]["id"]
+        if role == TrafficModel.CallsignRole:
+            return self._trafficEntries[row]["callsign"]
+        if role == TrafficModel.ModelRole:
+            return self._trafficEntries[row]["model"]
+        if role == TrafficModel.CategoryRole:
+            return self._trafficEntries[row]["category"]
+        if role == TrafficModel.LatitudeRole:
+            return self._trafficEntries[row]["latitude"]
+        if role == TrafficModel.LongitudeRole:
+            return self._trafficEntries[row]["longitude"]
+        if role == TrafficModel.AltitudeRole:
+            return self._trafficEntries[row]["altitude"]
+        if role == TrafficModel.TrackRole:
+            return self._trafficEntries[row]["track"]
+        if role == TrafficModel.GroundSpeedRole:
+            return self._trafficEntries[row]["groundSpeed"]
+        if role == TrafficModel.VerticalSpeedRole:
+            return self._trafficEntries[row]["verticalSpeed"]
+        if role == TrafficModel.SquawkRole:
+            return self._trafficEntries[row]["squawk"]
+        if role == TrafficModel.AlertRole:
+            return self._trafficEntries[row]["alert"]
+        if role == TrafficModel.EmergencyRole:
+            return self._trafficEntries[row]["emergency"]
+        if role == TrafficModel.SpiRole:
+            return self._trafficEntries[row]["spi"]
+        if role == TrafficModel.IsOnGroundRole:
+            return self._trafficEntries[row]["isOnGround"]
+        if role == TrafficModel.LastSeenRole:
+            return self._trafficEntries[row]["lastSeen"]
+        if role == TrafficModel.MsgCountRole:
+            return self._trafficEntries[row]["msgCount"]
+
+    def rowCount(self, parent=QModelIndex()):
+        return len(self._trafficEntries)
+
+    def roleNames(self):
+        return {
+            TrafficModel.IdRole: b"id",
+            TrafficModel.CallsignRole: b"callsign",
+            TrafficModel.ModelRole: b"model",
+            TrafficModel.CategoryRole: b"category",
+            TrafficModel.LatitudeRole: b"latitude",
+            TrafficModel.LongitudeRole: b"longitude",
+            TrafficModel.AltitudeRole: b"altitude",
+            TrafficModel.TrackRole: b"track",
+            TrafficModel.GroundSpeedRole: b"groundSpeed",
+            TrafficModel.VerticalSpeedRole: b"verticalSpeed",
+            TrafficModel.SquawkRole: b"squawk",
+            TrafficModel.AlertRole: b"alert",
+            TrafficModel.EmergencyRole: b"emergency",
+            TrafficModel.SpiRole: b"spi",
+            TrafficModel.IsOnGroundRole: b"isOnGround",
+            TrafficModel.LastSeenRole: b"lastSeen",
+            TrafficModel.MsgCountRole: b"msgCount",
+        }
+
+    @pyqtSlot(QVariant)
+    def addTrafficEntry(self, entry):
+        logger.info("add traffic entry")
+        self.beginInserRows(QModelIndex(), self.rowCount(), self.rowCount())
+        self._trafficEntries.append(entry)
+        self.endInsertRows()
+
+    @pyqtSlot(QVariant)
+    def updateTrafficEntry(self, entry):
+        row = self._rowFromId(entry["id"])
+        ix = self.index(row, 0)
+        changedRoles = []
+        if self._trafficEntries[row]["id"] != entry["id"]:
+            self._trafficEntries[row]["id"] = entry["id"]
+            changedRoles.append(TrafficModel.IdRole)
+        if self._trafficEntries[row]["callsign"] != entry["callsign"]:
+            self._trafficEntries[row]["callsign"] = entry["callsign"]
+            changedRoles.append(TrafficModel.CallsignRole)
+        if self._trafficEntries[row]["model"] != entry["model"]:
+            self._trafficEntries[row]["model"] = entry["model"]
+            changedRoles.append(TrafficModel.ModelRole)
+        if self._trafficEntries[row]["category"] != entry["category"]:
+            self._trafficEntries[row]["category"] = entry["category"]
+            changedRoles.append(TrafficModel.CategoryRole)
+        if self._trafficEntries[row]["latitude"] != entry["latitude"]:
+            self._trafficEntries[row]["latitude"] = entry["latitude"]
+            changedRoles.append(TrafficModel.LatitudeRole)
+        if self._trafficEntries[row]["longitude"] != entry["longitude"]:
+            self._trafficEntries[row]["longitude"] = entry["longitude"]
+            changedRoles.append(TrafficModel.LongitudeRole)
+        if self._trafficEntries[row]["altitude"] != entry["altitude"]:
+            self._trafficEntries[row]["altitude"] = entry["altitude"]
+            changedRoles.append(TrafficModel.AltitudeRole)
+        if self._trafficEntries[row]["track"] != entry["track"]:
+            self._trafficEntries[row]["track"] = entry["track"]
+            changedRoles.append(TrafficModel.TrackRole)
+        if self._trafficEntries[row]["groundSpeed"] != entry["groundSpeed"]:
+            self._trafficEntries[row]["groundSpeed"] = entry["groundSpeed"]
+            changedRoles.append(TrafficModel.GroundSpeedRole)
+        if self._trafficEntries[row]["verticalSpeed"] != entry["verticalSpeed"]:
+            self._trafficEntries[row]["verticalSpeed"] = entry["verticalSpeed"]
+            changedRoles.append(TrafficModel.VerticalSpeedRole)
+        if self._trafficEntries[row]["squawk"] != entry["squawk"]:
+            self._trafficEntries[row]["squawk"] = entry["squawk"]
+            changedRoles.append(TrafficModel.SquawkRole)
+        if self._trafficEntries[row]["alert"] != entry["alert"]:
+            self._trafficEntries[row]["alert"] = entry["alert"]
+            changedRoles.append(TrafficModel.AlertRole)
+        if self._trafficEntries[row]["emergency"] != entry["emergency"]:
+            self._trafficEntries[row]["emergency"] = entry["emergency"]
+            changedRoles.append(TrafficModel.EmergencyRole)
+        if self._trafficEntries[row]["spi"] != entry["spi"]:
+            self._trafficEntries[row]["spi"] = entry["spi"]
+            changedRoles.append(TrafficModel.SpiRole)
+        if self._trafficEntries[row]["isOnGround"] != entry["isOnGround"]:
+            self._trafficEntries[row]["isOnGround"] = entry["isOnGround"]
+            changedRoles.append(TrafficModel.IsOnGroundRole)
+        if self._trafficEntries[row]["lastSeen"] != entry["lastSeen"]:
+            self._trafficEntries[row]["lastSeen"] = entry["lastSeen"]
+            changedRoles.append(TrafficModel.LastSeenRole)
+        if self._trafficEntries[row]["msgCount"] != entry["msgCount"]:
+            self._trafficEntries[row]["msgCount"] = entry["msgCount"]
+            changedRoles.append(TrafficModel.MsgCountRole)
+
+        if len(changedRoles) > 0:
+            logger.info("update traffic entry {}".format(entry["id"]))
+        self.dataChanged.emit(ix, ix, changedRoles)
+
+    @pyqtSlot(int)
+    def removeTrafficEntry(self, id):
+        logger.info("remove traffic entry {}".format(id))
+        row = self._rowFromId(id)
+        self.beginRemoveRows(QModelIndex(), row, row)
+        del self._trafficEntries[row]
+        self.endRemoveRows()
+
+    @property
+    def ids(self) -> list():
+        return [t["id"] for t in self._trafficEntries]
+
+    def _rowFromId(self, id):
+        for index, item in enumerate(self._trafficEntries):
+            if item["id"] == id:
+                return index
+        raise ValueError("no traffic entry for id {}".format(id))
+
+
 app = QGuiApplication(sys.argv)
 satellitesModel = SatellitesModel()
 positionModel = PositionModel()
+trafficModel = TrafficModel()
 engine = QQmlApplicationEngine()
 engine.rootContext().setContextProperty("satellitesModel", satellitesModel)
 engine.rootContext().setContextProperty("positionModel", positionModel)
+engine.rootContext().setContextProperty("trafficModel", trafficModel)
 engine.quit.connect(app.quit)
 engine.load("main.qml")
 mqttClient = mqtt.launch("easyadsb-gui", "localhost", 1883)
