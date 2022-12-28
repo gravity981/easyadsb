32806c5e10ddfd66fdd07a66dc84a3a09562cd50
gravity981 <gravity981@noreply.com>
2022-12-28 12:09:01 +0100

    remove global variables

--- core/bme280/app/main.py
+++ core/bme280/app/main.py
@@ -1,6 +1,6 @@
 import bme280
 import smbus2
-import logging
+import logging as log
 import atexit
 import os
 import uuid
@@ -15,20 +15,19 @@ except ImportError:
     import logconf
 
 
-def on_exit():
-    if logger is not None:
-        logger.info("Exit application")
+def onExit(mqClient):
+    log.info("Exit application")
     if mqClient is not None:
         mqClient.disconnect()
 
 
 def calculatePressureAltitude(pressure, temperature) -> float:
     referencePressure = 1013.25  # hPa
-    # hypsometric formula, should work for altitudes up to 11km
+    # hypsometric formula, should be accurate enough for altitudes up to 11km
     return (((referencePressure / pressure) ** (1 / 5.257) - 1) * (temperature + 273.15)) / 0.0065
 
 
-def run_periodic_publish(mqClient, bus, address):
+def run_periodic_publish(mqClient, bus, address, calibration_params, publish_topic):
     while True:
         data = bme280.sample(bus, address, calibration_params)
         obj = dict()
@@ -37,17 +36,14 @@ def run_periodic_publish(mqClient, bus, address):
         obj["temperature"] = round(data.temperature, 3)  # °C
         obj["pressureAltitude"] = round(calculatePressureAltitude(data.pressure, data.temperature), 3)  # m
         js = json.dumps(obj)
-        logger.debug(js)
+        log.debug(js)
         mqClient.publish(publish_topic, js)
         time.sleep(1)
 
 
-if __name__ == "__main__":
-    logger = None
+def main():
     mqClient = None
-    run = True
 
-    logger_name = "logger"
     i2c_port = int(os.getenv("BM_I2C_PORT"))
     i2c_address = int(os.getenv("BM_I2C_ADDRESS"), 16)
     log_level = str(os.getenv("BM_LOG_LEVEL"))
@@ -57,17 +53,20 @@ if __name__ == "__main__":
     publish_topic = str(os.getenv("BM_MQTT_PUBLISH_TOPIC"))
 
     logconf.setup_logging(log_level)
-    logger = logging.getLogger(logger_name)
-    atexit.register(on_exit)
 
     if client_name == "":
-        logger.info("client_name is empty, assign uuid")
+        log.info("client_name is empty, assign uuid")
         client_name = str(uuid.uuid1())
 
     mqClient = mqtt.launch(client_name, broker, port, [], None)
+    atexit.register(onExit, mqClient)
 
     # BME280 sensor stuff
     bus = smbus2.SMBus(i2c_port)
     calibration_params = bme280.load_calibration_params(bus, i2c_address)
-    logger.info("start publishing i2c messages to {topic}".format(topic=publish_topic))
-    run_periodic_publish(mqClient, bus, i2c_address)
+    log.info("start publishing i2c messages to {topic}".format(topic=publish_topic))
+    run_periodic_publish(mqClient, bus, i2c_address, calibration_params, publish_topic)
+
+
+if __name__ == "__main__":
+    main()
